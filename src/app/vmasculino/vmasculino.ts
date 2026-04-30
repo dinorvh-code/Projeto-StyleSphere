d61ecf66d462eaae4309714cecd1e9c6d4c9288f
dnorvh-code <dinorvh@gmail.com>
2026-04-30 16:01:27 -0500

    Ok ta indo

--- src/app/vmasculino/vmasculino.ts
+++ src/app/vmasculino/vmasculino.ts
@@ -14,30 +14,99 @@ elementos = [
 {
   nome: 'Camisa Polo Preta',
   imagem: 'assets/images/vmasculino/camisa-polo-preta.png',
-  $anterior: 'R$ 00,00',
+  $posterior: 'R$ 00,00'
+},
+
+{
+  nome: 'Calca Jeans Cargo',
+  imagem: 'assets/images/vmasculino/calca-jeans-cargo.png',
+  $posterior: 'R$ 00,00'
+},
+
+{
+  nome: 'Bota Social Marrom',
+  imagem: 'assets/images/vmasculino/bota-social-marrom.png',
+  $posterior: 'R$ 00,00'
+},
+
+{
+  nome: 'Relógio Prata Escuro',
+  imagem: 'assets/images/vmasculino/relogio-prata-escuro.png',
+  $posterior: 'R$ 00,00'
+},
+
+{
+  nome: 'Oculos Dourado Azul',
+  imagem: 'assets/images/vmasculino/oculos-dourado-azul.png',
+  $posterior: 'R$ 00,00'
+},
+
+{
+  nome: 'Sapato Social Preto',
+  imagem: 'assets/images/vmasculino/sapato-social-preto.png',
+  $posterior: 'R$ 00,00'
+},
+
+{
+  nome: 'Calça Social Preta',
+  imagem: 'assets/images/vmasculino/calca-social-preta.png',
   $posterior: 'R$ 00,00'
 },
 
 {
   nome: 'Paletó Cinza',
   imagem: 'assets/images/vmasculino/paleto-cinza.png',
-  $anterior: 'R$ 00,00',
   $posterior: 'R$ 00,00'
 },
 
 {
-  nome: 'Camisa Polo Branca',
-  imagem: 'assets/images/vmasculino/camisa-polo-branca.png',
-  $anterior: 'R$ 00,00',
+  nome: 'x',
+  imagem: 'assets/images/vmasculino/x.png',
   $posterior: 'R$ 00,00'
 },
 
 {
-  nome: 'Camisa Social Verde',
-  imagem: 'assets/images/vmasculino/camisa-social-verde.png',
-  $anterior: 'R$ 00,00',
+  nome: 'x',
+  imagem: 'assets/images/vmasculino/x.png',
   $posterior: 'R$ 00,00'
-}
+},
+
+{
+  nome: 'x',
+  imagem: 'assets/images/vmasculino/x.png',
+  $posterior: 'R$ 00,00'
+},
+
+{
+  nome: 'x',
+  imagem: 'assets/images/vmasculino/x.png',
+  $posterior: 'R$ 00,00'
+},
+
+{
+  nome: 'x',
+  imagem: 'assets/images/vmasculino/x.png',
+  $posterior: 'R$ 00,00'
+},
+
+{
+  nome: 'x',
+  imagem: 'assets/images/vmasculino/x.png',
+  $posterior: 'R$ 00,00'
+},
+
+{
+  nome: 'x',
+  imagem: 'assets/images/vmasculino/x.png',
+  $posterior: 'R$ 00,00'
+},
+
+{
+  nome: 'x',
+  imagem: 'assets/images/vmasculino/x.png',
+  $posterior: 'R$ 00,00'
+},
+
 
 ]
 
